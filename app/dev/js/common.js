0dfa794aff49ea713e55a0bf74b4955723b1c49c
堀口誠人 <horiguchi_seito@liginc.co.jp>
2015-01-26 20:22:45 +0900

    SP部分完成

--- app/dev/js/common.js
+++ app/dev/js/common.js
@@ -2,5 +2,31 @@
 
     'use strict';
 
-})();
+    var SPEED = 350;
 
+    var $trigger = $('#js-trigger'),
+        $wrapper = $('#js-wrapper'),
+        $nav = $('#js-nav');
+
+    //header nav button
+    $trigger.on('click',function(){
+
+        if(!$trigger.hasClass('is-active')) {
+            $trigger.addClass('is-active');
+            $wrapper.fadeOut(SPEED);
+            $nav.fadeIn(SPEED);
+        } else {
+            $trigger.removeClass('is-active');
+            $wrapper.fadeIn(SPEED);
+            $nav.fadeOut(SPEED);
+        }
+    });
+
+    //nav close button
+    $('#js-close').on('click',function(){
+        $trigger.removeClass('is-active');
+        $wrapper.fadeIn(SPEED);
+        $nav.fadeOut(SPEED);
+    });
+
+})();
